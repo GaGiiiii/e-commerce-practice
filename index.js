200339a1a44da1034b0566735cdc8035847e27de
GaGiiiii <dragoslav.gagi8@gmail.com>
2020-09-12 22:01:33 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    // BANNER OWL CAROUSEL
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots: true,
+        items: 1
+    });
+
+    // TOP SALE OWL CAROUSEL
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+    // ISOTOP
+    var $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
+
+    // FILTER ITEMS ON BUTTON CLICK
+    $(".button-group").on('click', "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+});
